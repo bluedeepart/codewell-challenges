094a89c8203b01fffde40077762ed87f93ef7b2b
Deepti Rathor <deepti.rathor46@gmail.com>
2022-01-07 18:48:41 +0530

    contrast delivery project added

---
+++ 2022/contrast-directory/js/app.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const showResourceData = async (resourseFolder, resourcePath, root) => {
+    const response = await fetch(
+      `/2022/contrast-directory/images/Resources/${resourseFolder}/${resourcePath}`
+    );
+    const allData = await response.json();
+    const rootHtml = document.getElementById(root);
+    for (let d in allData) {
+    //   console.log(allData[d]);
+      let output = `<div class="col-lg-4 col-md-6 col-sm-12">
+             <div class="card">
+                 <div class="card-body">
+                     <div><img src="images/Resources/${resourseFolder}/${allData[d].Icon}" alt="google" class="mb-2">
+                     <h3>${allData[d].Title}</h3>
+                     <p>${allData[d].Description}</p></div>
+                     <p class="mb-0"><strong><a href=${allData[d].Link} target="_blank" rel="noopener noreferrer">Visit Resource →</a></strong></p>
+                 </div>
+             </div>
+         </div>`;
+      rootHtml.innerHTML += output;
+    }
+  };
+
+  showResourceData("Fonts","fonts.json", "fontResourse");
+  showResourceData("Mockups","mockups.json", "mockupResourse");
+  showResourceData("Illustrations","illustrations.json", "illustrationResourse");
+  showResourceData("Icons","icons.json", "iconResourse");
+});
